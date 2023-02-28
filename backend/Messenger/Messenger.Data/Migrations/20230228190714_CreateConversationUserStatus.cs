93105d3d60dd2bfe9f7c674c1b9167027debd80e
selezen4ever <apdasd@mail.ru>
2023-02-28 23:32:05 +0300

    Create "ConversationUserStatus" entity && migrate

---
+++ backend/Messenger/Messenger.Data/Migrations/20230228190714_CreateConversationUserStatus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Messenger.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateConversationUserStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ConversationUserStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    ConversationId = table.Column<Guid>(type: "uuid", nullable: true),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ReadTo = table.Column<long>(type: "bigint", nullable: true),
+                    DeletedTo = table.Column<long>(type: "bigint", nullable: true),
+                    IsDeletedByUser = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConversationUserStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConversationUserStatuses_ConversationId_UserId",
+                table: "ConversationUserStatuses",
+                columns: new[] { "ConversationId", "UserId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConversationUserStatuses");
+        }
+    }
+}
