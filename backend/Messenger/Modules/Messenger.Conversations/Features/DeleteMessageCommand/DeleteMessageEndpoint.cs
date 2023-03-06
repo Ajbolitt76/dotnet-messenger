910478c84c915290c4e181b7af62a58e4e0ae284
selezen4ever <apdasd@mail.ru>
2023-03-06 14:45:41 +0300

    Create delete message endpoint && enable private messages deletion

---
+++ backend/Messenger/Modules/Messenger.Conversations/Features/DeleteMessageCommand/DeleteMessageEndpoint.cs
@@ -0,0 +1,22 @@
+using MediatR;
+using Messenger.Infrastructure.Endpoints;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Messenger.Conversations.Features.DeleteMessageCommand;
+
+public class DeleteMessageEndPoint : IEndpoint
+{
+    public void Map(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost(
+                "/deleteMessage/{messageId:guid}",
+                async (Guid messageId, bool deleteFromAll, IMediator mediator)
+                    => Results.Ok(
+                        await mediator.Send(
+                            new DeleteMessageCommand(messageId, deleteFromAll))))
+            .RequireAuthorization()
+            .WithName("Удалить сообщение из переписки");
+    }
+}
