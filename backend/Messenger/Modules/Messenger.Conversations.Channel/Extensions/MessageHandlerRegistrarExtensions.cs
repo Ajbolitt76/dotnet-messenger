45ca7722b5e0cfbe89c98779c723af32cf942fac
W1ngshot <kirill-samsonov@bk.ru>
2023-05-14 15:24:03 +0300

    channel get messages done

--- backend/Messenger/Modules/Messenger.Conversations.Channel/Extensions/MessageHandlerRegistrarExtensions.cs
+++ backend/Messenger/Modules/Messenger.Conversations.Channel/Extensions/MessageHandlerRegistrarExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Messenger.Conversations.Channel.MessageActions.ChannelSendMessage;
+﻿using Messenger.Conversations.Channel.MessageActions.ChannelGetMessageList;
+using Messenger.Conversations.Channel.MessageActions.ChannelSendMessage;
+using Messenger.Conversations.Common.MessageActions.GetMessageList;
 using Messenger.Conversations.Common.MessageActions.SendMessage;
 using Messenger.Conversations.Common.Services;
 
@@ -8,7 +10,7 @@ public static class MessageHandlerRegistrarExtensions
 {
     public static MessageHandlerRegistrar AddChannelMessageHandlers(this MessageHandlerRegistrar registrar)
     {        
-        //registrar.AddHandler<GroupGetMessageListActionHandler, GetMessageListAction, GetMessageListActionResponse>();
+        registrar.AddHandler<ChannelGetMessageListActionHandler, GetMessageListAction, GetMessageListActionResponse>();
         registrar.AddHandler<ChannelSendMessageActionHandler, SendMessageAction, SendMessageActionResponse>();
         //registrar.AddHandler<GroupDeleteMessageActionHandler, DeleteMessageAction, bool>();
         return registrar;
